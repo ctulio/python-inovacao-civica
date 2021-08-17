bcccea70de4979f3dfa0d4e12c61510fd6f5d9c5
César Túlio Albuquerque de Almeida <cesar-tulio@hotmail.com>
2021-08-17 16:40:02 -0400

    modulo 1 finalizado

---
+++ ctulio/1.3.py
@@ -0,0 +1,19 @@
+''' # Trabalhando com diferença de versão
+try:
+    nome = raw_input('Digite seu nome: ') #python2
+except:
+    nome = input('Digite seu nome: ')     #python3
+    idade = input('Digite sua idade: ')
+'''
+
+sg_partido = ['PSOL', 'PTB', 'MDB', 'ARENA']
+sg_partido_b = []
+sg_partido_indefinida = []
+
+for sigla in sg_partido:
+    if sigla[0] == 'P':
+        sg_partido_b.append(sigla)
+    else:
+        sg_partido_indefinida.append(sigla)
+
+print(sg_partido_b, sg_partido_indefinida)
