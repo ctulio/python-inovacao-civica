62e25ab14e3ac7db7b9272216968a0eae559df49
César Túlio Albuquerque de Almeida <cesar-tulio@hotmail.com>
2021-09-04 22:20:11 -0400

    ajusta arquivo

--- ctulio/1.3.py
+++ ctulio/1.3.py
@@ -1,6 +1,7 @@
 ''' # Trabalhando com diferença de versão
 try:
-    nome = raw_input('Digite seu nome: ') #python2
+    nome = raw_input('Digite seu nome: ') #python2  
+    ## função input(), no Python 2, não transforma a entrada do usuário em string, mas tenta avaliar, calcular ela
 except:
     nome = input('Digite seu nome: ')     #python3
     idade = input('Digite sua idade: ')
