e6bde51bda362af2fbea5b68664749811e1e7488
César Túlio Albuquerque de Almeida <cesar-tulio@hotmail.com>
2021-10-02 16:56:45 -0400

    Ajusta arquivo regex

--- ctulio/s05-05-regex.py
+++ ctulio/s05-05-regex.py
@@ -11,3 +11,24 @@ print(reticencias)
 
 pontos = re.findall(r"\.", texto)
 print(pontos)
+
+
+
+# ============================================
+l = ('89', '85', '1988','pastel', 'pato', \
+	 '88', '859', '78','1932', '65',\
+	 '82', '1981')
+
+s = r'198[0-9]{1}'
+# s = r'p.*'
+
+# Ocorre erro na impressão quando não encontra um padrão
+# for num in l:
+# 	print(re.search(s, num).group())
+
+
+# O if funciona como um teste, evitando o erro
+# caso não exista o padrão buscado
+for num in l:
+	if re.search(s, num):
+		print(re.search(s, num).group())
